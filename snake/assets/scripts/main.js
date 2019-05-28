6adab9fc4bd11b3b30cb2a1979709d28b2ddedcc
Vladyslav <botaniqyes@gmail.com>
2019-05-28 14:16:48 +0300

    Added  class Block with drawCircle and drawSquare methods

--- snake/assets/scripts/main.js
+++ snake/assets/scripts/main.js
@@ -38,13 +38,52 @@ function gameOver() {
     ctx.fillText('Game over!', width / 2, height / 2);
 }
 
-drawBorder();
-drawScore();
-gameOver();
+// drawBorder();
+// drawScore();
+// gameOver();
 
-setInterval(function() {
-    score++;
-    ctx.clearRect(0, 0, width, height);
-    drawBorder();
-    drawScore();
-}, 100)
+// setInterval(function() {
+//     score++;
+//     ctx.clearRect(0, 0, width, height);
+//     drawBorder();
+//     drawScore();
+// }, 100)
+
+
+
+
+
+
+class Block {
+    constructor(col, row) {
+        this.col = col;
+        this.row = row;
+    }
+
+    drawSquare(color) {
+        let x = this.col * blockSize,
+            y = this.row * blockSize;
+        
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, blockSize, blockSize);
+    }
+
+    drawCircle(color) {
+        let centerX = this.col * blockSize + blockSize / 2,
+            centerY = this.row * blockSize + blockSize / 2;
+        
+        ctx.fillStyle = color;
+        circle(centerX, centerY, blockSize / 2, true);
+    }
+}
+
+
+let block = new Block(4, 4);
+
+console.log(block);
+
+block.drawSquare('red');
+
+
+let circle = new Block(4, 3);
+circle.drawCircle('Green');
