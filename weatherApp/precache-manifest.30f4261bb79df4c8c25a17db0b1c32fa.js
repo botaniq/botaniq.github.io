5cec2af168a1213fed8ccf5da246e9ebf8b6d385
Vladyslav <botaniqyes@gmail.com>
2019-07-09 16:10:34 +0300

    Added weatherApp

---
+++ weatherApp/precache-manifest.30f4261bb79df4c8c25a17db0b1c32fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f24bd41ba62799bd6004bcf9eb4d93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7360b7fadc03e6704e8",
+    "url": "/static/css/main.48416d7b.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "b7360b7fadc03e6704e8",
+    "url": "/static/js/main.77953f04.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
