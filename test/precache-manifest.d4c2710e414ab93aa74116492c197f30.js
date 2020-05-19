2c2d902890ce0b0dcc2c5db772598a7f154c2c17
botaniq <botaniqyes@gmail.com>
2020-05-19 21:12:28 +0300

    - added test

---
+++ test/precache-manifest.d4c2710e414ab93aa74116492c197f30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdfb7539a67f21f4ef4ba3832cf5289a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67ffbf6906230c9bff96",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "a176cc3dee2c85713e96",
+    "url": "/static/css/main.9f47be17.chunk.css"
+  },
+  {
+    "revision": "67ffbf6906230c9bff96",
+    "url": "/static/js/2.8f44ebe6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8f44ebe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a176cc3dee2c85713e96",
+    "url": "/static/js/main.379296a2.chunk.js"
+  },
+  {
+    "revision": "2aae4f5bb94a3d3a590d",
+    "url": "/static/js/runtime-main.c9686b65.js"
+  }
+]);
